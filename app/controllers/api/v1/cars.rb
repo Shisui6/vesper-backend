dabcf53b44a0dce02da57f31b93993559e919fab
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-03 15:51:05 +0430

    Fix errors

--- app/controllers/api/v1/cars.rb
+++ app/controllers/api/v1/cars.rb
@@ -2,19 +2,10 @@ class Api::V1::CarsController < ApplicationController
   before_action :set_cars, only: %i[show destroy]
   before_action :set_user, only: %i[create destroy]
 
-  # GET /cars
-  def index 
-
   # GET /cars/1
   def show
     render json: @car
   end
-
-  # POST /cars
-  def create
-
-  # DELETE /cars/1
-  def destroy
 
   private
 
@@ -31,3 +22,4 @@ class Api::V1::CarsController < ApplicationController
   def car_params
     params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
   end
+end
