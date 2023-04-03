c88217212e63a63274e06863351c4ad3352ae154
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-03 15:47:59 +0430

    fix errors

--- app/controllers/api/v1/cars.rb
+++ app/controllers/api/v1/cars.rb
@@ -31,6 +31,3 @@ class Api::V1::CarsController < ApplicationController
   def car_params
     params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
   end
-end
-end
-end
