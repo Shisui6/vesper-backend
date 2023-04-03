1dd9d39cc076387ef87978218b3fd78b09cfcd8d
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-03 15:04:59 +0430

    Add cars params

--- app/controllers/api/v1/cars.rb
+++ app/controllers/api/v1/cars.rb
@@ -30,4 +30,9 @@ class Api::V1::carsController < ApplicationController
   def set_user
     @user = current_user
   end
+
+  # Only allow a list of trusted parameters through.
+  def car_params
+    params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
+  end
 end
