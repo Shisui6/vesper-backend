481c2e66078cd095a15df29b8f72257f40550402
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-04-03 13:07:29 +0100

    Update cars.rb

--- app/controllers/api/v1/cars.rb
+++ app/controllers/api/v1/cars.rb
@@ -1,24 +1,12 @@
 class Api::V1::CarsController < ApplicationController
-  before_action :set_cars, only: %i[show]
-
   # GET /cars/1
   def show
+    car = Car.find(params[:id])
+
     if car
       render json: car, status: :ok
     else
       render json: car.errors, status: :bad_request
     end
   end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_car
-    car = Car.find(params[:id])
-  end
-
-  # # Only allow a list of trusted parameters through.
-  # def car_params
-  #   params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
-  # end
 end
