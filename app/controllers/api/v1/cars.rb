9f21cc51183c3d7d59c6e9897b8171a4a7e37f98
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-03 16:24:42 +0430

    Add requisted changes

--- app/controllers/api/v1/cars.rb
+++ app/controllers/api/v1/cars.rb
@@ -1,25 +1,24 @@
 class Api::V1::CarsController < ApplicationController
-  before_action :set_cars, only: %i[show destroy]
-  before_action :set_user, only: %i[create destroy]
+  before_action :set_cars, only: %i[show]
 
   # GET /cars/1
   def show
-    render json: @car
+    if car
+      render json: car, status: :ok
+    else
+      render json: car.errors, status: :bad_request
+    end
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_car
-    @car = car.find(params[:id])
+    car = Car.find(params[:id])
   end
 
-  def set_user
-    @user = current_user
-  end
-
-  # Only allow a list of trusted parameters through.
-  def car_params
-    params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
-  end
+  # # Only allow a list of trusted parameters through.
+  # def car_params
+  #   params.require(:car).permit(:name, :description, :image, :type, :model, :year, :price_per_day, :user_id)
+  # end
 end
