695c87eb867bcfb0ea45758d25afafbb7fcc8a10
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-04-04 21:05:17 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cars, only: [:show]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :reservations, only: [:index :create]
     end
   end
+
   resources :users
   post '/auth/login', to: 'authentication#login'
 end
