77d2af5c776f9469a70712360dfbf20e7ceda557
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-04-04 20:31:45 +0100

    Fix error

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationController < ApplicationController
 
     if @user&.authenticate(params[:password])
       token = jwt_encode(user_id: @user.id)
-      render json: { token: token, user: @user }, status: :ok
+      render json: { token:, user: @user }, status: :ok
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
