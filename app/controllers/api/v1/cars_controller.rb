25aac67b8f07bc4e5a250c8d6fd7799e6b6a7f7e
Usirusen Ikon <lexicon048@gmail.com>
2023-04-10 12:26:09 +0100

    Update create car method

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -19,7 +19,6 @@ class Api::V1::CarsController < ApplicationController
   # POST /car/1/rservation
   def create
     @car = Car.new(car_params)
-    @car.user = @user
     if @car.save
       render json: {
         status: { success: true, message: 'Car created successfully' }
@@ -41,10 +40,12 @@ class Api::V1::CarsController < ApplicationController
 
   private
 
+  def set_user
+    @user = current_user
+  end
+
   # Only allow a list of trusted parameters through.
   def car_params
-    params.require(:car).permit(
-      :name, :description, :image, :type, :model, :year, :price_per_day
-    )
+    params.permit(:name, :description, :image, :classification, :model, :year, :price_per_day, :user_id)
   end
 end
